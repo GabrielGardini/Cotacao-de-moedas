5326a520dcf8c278019928fdde9b3fcaab3fbdc9
Gabriel Gardini <g246289@dac.unicamp.br>
2021-08-26 17:23:12 -0300

    adiciona a função de converção de moedas

--- scripts.js
+++ scripts.js
@@ -1,3 +1,13 @@
+// const converted_dol = document.querySelector('txt_dolar');
+// const converted_euro = document.querySelector('_euro');
+// const converted_btc = document.querySelector('#span_btc');
+
+
+
+
+
+
+
 function applyCot(dollar, euro, bitcoin, var_dolar, var_euro, var_btc) {
     /*
     Apply the values ​​received by the API to the HTML.
@@ -35,6 +45,28 @@ function requestAPI(url) {
     return JSON.parse(request.responseText);
 }
 
+function convert_dol(){
+    const inputed_dol = document.getElementById('inputed_dol').value;
+    const coinData = requestAPI("https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL");
+    var dolar = coinData["USDBRL"]["bid"];
+    var result = dolar*inputed_dol;
+    document.getElementById('txt_dolar').innerText="R$ " + result.toFixed(2);
+};
+function convert_euro(){
+    const inputed_euro = document.getElementById('inputed_euro').value;
+    const coinData = requestAPI("https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL");
+    var euro = coinData["EURBRL"]["bid"];
+    var result = euro*inputed_euro;
+    document.getElementById('txt_euro').innerText="R$ " + result.toFixed(2);
+}
+function convert_btc(){
+    const inputed_btc = document.getElementById('inputed_btc').value;
+    const coinData = requestAPI("https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL");
+    var btc = coinData["BTCBRL"]["bid"];
+    var result = btc*inputed_btc;
+    document.getElementById('txt_btc').innerText="R$ " + result.toFixed(2);
+}
+
 function main() {
     const coinData = requestAPI(
         "https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL"
@@ -54,4 +86,4 @@ main();
 
 
 // updates every 1 second
-setInterval(main, 30000)
+setInterval(main, 1000)
